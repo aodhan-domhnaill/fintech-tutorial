96dd2edc532284c57e50aa7e78fc5c697d982d48
Aidan Plenert Macdonald <training@nobleprog.us>
2023-11-17 09:23:12 -0500

    Supports multiple lambdas

--- compute/support.py
+++ compute/support.py
@@ -2,7 +2,8 @@ import ctypes
 import sys
 import os 
 
-
+# https://numpy.org/doc/stable/reference/generated/numpy.ndarray.ctypes.html
+# https://numpy.org/doc/stable/reference/routines.ctypeslib.html
 class Support:
     def __init__(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
